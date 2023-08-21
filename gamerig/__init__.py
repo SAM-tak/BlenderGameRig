32e87613fedde6cfbedd15c8743119e9c40af132
SAM-tak <osamu.takasugi@gmail.com>
2023-08-21 16:27:51 +0900

    bump version

--- gamerig/__init__.py
+++ gamerig/__init__.py
@@ -20,7 +20,7 @@
 
 bl_info = {
     "name": "GameRig",
-    "version": (1, 5, 0),
+    "version": (1, 6, 0),
     "author": "Osamu Takasugi, (Rigify : Nathan Vegdahl, Lucio Rossi, Ivan Cappiello)",
     "blender": (3, 1, 0),
     "description": "Character Rigging framework for Game / Realtime content",
